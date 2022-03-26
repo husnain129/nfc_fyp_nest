3fc6a7a8d48713d28325b8d73bd53566bbb0a167
Muhammad Husnain <54207663+m-h9@users.noreply.github.com>
2022-03-26 15:54:25 +0500

    complete upload image to server

--- src/Image/Image.controller.ts
+++ src/Image/Image.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { diskStorage } from 'multer';
+import { nanoid } from 'nanoid';
 
 @Controller('image')
-export class ImageController {}
+export class ImageController {
+  @Post()
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: diskStorage({
+        destination: './uploads/images',
+        filename: (req, file, cb) => {
+          const filename: string = file.originalname.split('.')[0] + nanoid(4);
+          const extension: string = file.originalname.substring(
+            file.originalname.lastIndexOf('.'),
+            file.originalname.length,
+          );
+          cb(null, `${filename}${extension}`);
+        },
+      }),
+    }),
+  )
+  uploadImage(@UploadedFile() file: Express.Multer.File): {
+    imagePath: string;
+  } {
+    return { imagePath: file.filename };
+  }
+}
